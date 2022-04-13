6b1e9df0a5274eaf8e10e61194173aa65a7cb7b1
User <User@DESKTOP-802SV85>
2022-04-13 19:41:12 +0300

    task3 done

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 int main()
 {
-   
+	setlocale(0, "");
+	int a = 0;
+	int* a2 = &a;
+	int b = 0;
+	int* b2 = &b;
+
+	cout << "введите число 1 :";
+	cin >> *a2;
+	cout << "введите число 2 :";
+	cin >> *b2;
+
+	a = *a2;
+	b = *b2;
+
+	b2 = new int(a);
+	a2 = new int(b);
+	cout << "число 1 :" << *a2 << endl;
+	cout << "число 2 :" << *b2;
+	
 }
