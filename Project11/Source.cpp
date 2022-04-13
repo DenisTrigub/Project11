9da47caa5d1698d7524c3ca6dbe5bad7042f9456
User <User@DESKTOP-802SV85>
2022-04-13 19:08:07 +0300

    task 1 done

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 int main()
 {
-   
+	setlocale(0, "");
+	int a = 0;
+	int* a2 = &a;
+	int b = 0;
+	int* b2 = &b;
+	
+	cout << "введите число 1 :";
+	
+	cin >> *a2;
+	cout << "введите число 2 :";
+	cin >> *b2;
+	if (*a2 < *b2)
+	{
+		cout << "число 2 больше"<< endl;
+		cout << *a2 << "<" << *b2;
+	}
+	if (*a2 > *b2)
+	{
+		cout << "число 1 больше" << endl;
+		cout << *a2 << ">" << *b2;
+	}
 }
