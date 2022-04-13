9125edaf67596ec40475fe3db3fd5113160766ff
User <User@DESKTOP-802SV85>
2022-04-13 19:18:35 +0300

    task 2 done

--- Project11/Source.cpp
+++ Project11/Source.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 int main()
 {
-   
+	setlocale(0, "");
+	int a = 0;
+	int* a2 = &a;
+
+	cout << "введите число :";
+	cin >> *a2;
+	
+	if (0 > *a2)
+	{
+		cout << "число отрицательное " << endl;
+		cout << "0 >" << *a2;
+	}
+	if (0 < *a2)
+	{
+		cout << "число положительное " << endl;
+		cout << "0 <" << *a2;
+	}
 }
